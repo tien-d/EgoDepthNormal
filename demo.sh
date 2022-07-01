314a13fa2aa7f013bf421a54b7b78b76918c72b4
vuong067 <vuong067@umn.edu>
2022-06-30 23:13:02 -0500

    prepare to add files back from master

--- demo.sh
+++ demo.sh
@@ -3,11 +3,11 @@ python main_depth.py --train 0 --model_type 'midas_v21' --dataset_type 'demo' \
 --input_dir ./demo_data/color \
 --checkpoint /mars/mnt/oitstorage/khiem_storage/workspace/CVPR22/result/depth_camready_midasv21_baseline_midasloss_randomcropresize_fixdata/model-epoch-00007-iter-42000.ckpt \
 --batch_size 4 --skip_every_n_image_test 1 \
---save_visualization /mars/mnt/oitstorage/khiem_storage/output_midas/cvpr22/test_demodata_depth
+--save_visualization ./demo_visualization/depth_results
 
 ##### Run normal demo ####
 python main_normal.py --train 0 --model_type 'midas_v21' --dataset_type 'demo' \
 --input_dir ./demo_data/color \
---checkpoint /mars/mnt/oitstorage/khiem_storage/workspace/CVPR22/result/sne_camready_midasv21_baseline/model-epoch-00013-iter-02000.ckpt \
+--checkpoint /mars/mnt/oitstorage/khiem_storage/workspace/CVPR22/result/normal_camready_midasv21_baseline_midasloss_randomcropresize_fixdata/model-epoch-00011-iter-02000.ckpt \
 --batch_size 4 --skip_every_n_image_test 1 \
 --save_visualization /mars/mnt/oitstorage/khiem_storage/output_midas/cvpr22/test_demodata_normal
--- eval.sh
+++ eval.sh
@@ -5,13 +5,13 @@ python main_depth.py --train 0 --model_type 'midas_v21' \
 --dataset_pickle_file /mars/home/khiem/projects/ongoing_exps/EgoDepthNormal_Code/dataset_creators/scannet_edina_camready_final_clean.pkl \
 --batch_size 8 --skip_every_n_image_test 40 \
 --data_root /mars/mnt/oitstorage/EDINA_pruned \
-#--save_visualization /mars/mnt/oitstorage/khiem_storage/output_midas/cvpr22/test_eval_release_depth
+--save_visualization /mars/mnt/oitstorage/khiem_storage/output_midas/cvpr22/test_eval_release_depth
 
-##### Run normal evaluation ####
-#python main_normal.py --train 0 --model_type 'midas_v21' \
-#--test_usage 'edina_test' \
-#--checkpoint /mars/mnt/oitstorage/khiem_storage/workspace/CVPR22/result/normal_camready_midasv21_baseline_midasloss_randomcropresize_fixdata/model-epoch-00011-iter-02000.ckpt \
-#--dataset_pickle_file /mars/home/khiem/projects/ongoing_exps/EgoDepthNormal_Code/dataset_creators/scannet_edina_camready_final_clean.pkl \
-#--batch_size 8 --skip_every_n_image_test 40 \
-#--data_root /mars/mnt/oitstorage/EDINA_pruned \
-#--save_visualization /mars/mnt/oitstorage/khiem_storage/output_midas/cvpr22/test_eval_release_normal
+#### Run normal evaluation ####
+python main_normal.py --train 0 --model_type 'midas_v21' \
+--test_usage 'edina_test' \
+--checkpoint /mars/mnt/oitstorage/khiem_storage/workspace/CVPR22/result/normal_camready_midasv21_baseline_midasloss_randomcropresize_fixdata/model-epoch-00011-iter-02000.ckpt \
+--dataset_pickle_file /mars/home/khiem/projects/ongoing_exps/EgoDepthNormal_Code/dataset_creators/scannet_edina_camready_final_clean.pkl \
+--batch_size 8 --skip_every_n_image_test 40 \
+--data_root /mars/mnt/oitstorage/EDINA_pruned \
+--save_visualization /mars/mnt/oitstorage/khiem_storage/output_midas/cvpr22/test_eval_release_normal
